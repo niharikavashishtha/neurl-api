631bd561d09b846dbbaecd38ba2fc608772bb91a
niharikavashishtha <niharikavashishtha@gmail.com>
2021-07-01 12:57:06 +0530

    my first commit

---
+++ src/main/java/com/neueda/neurl/NeurlApiApplication.java
@@ -0,0 +1,13 @@
+package com.neueda.neurl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeurlApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeurlApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neueda/neurl/NeurlApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neueda.neurl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeurlApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
